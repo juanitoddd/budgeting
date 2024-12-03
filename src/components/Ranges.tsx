29636330489e1fbd9869381cd69b68d2f53d1e81
Juan David <juan.diaz@23degrees.io>
2024-12-03 10:09:12 +0100

    UPDATE

--- src/components/Ranges.tsx
+++ src/components/Ranges.tsx
@@ -1,37 +1,87 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { ConfigProvider, Slider } from 'antd';
 import { cyan, magenta, gold, volcano } from '@ant-design/colors';
 
-const categories: any[] = [
-  {id:'fixed', label:'Fixed costs', color: cyan, value:[0,50]}, 
-  {id:'savings', label:'Savings', color: magenta, value:[50,60]}, 
-  {id:'investment', label:'Investment', color: gold, value:[60,70]}, 
-  {id:'free', label:'Free guilt spending', color: volcano, value:[70,100]}
-]
+interface Range {
+  id: string,
+  label: string,
+  color: string[],
+  value: number[]
+}
 
 export function Ranges() {
-  const onChange = (value: number | number[], index: number) => {
-    console.log('onChange: ', value, index);
-  };
   
-  // const { useToken } = antdTheme
-  // const { token: theme } = useToken() 
+
+  let defaultRanges: Range[] = [
+    {id:'fixed', label:'Fixed costs', color: cyan, value:[0,50]}, 
+    {id:'savings', label:'Savings', color: magenta, value:[50,60]}, 
+    {id:'investment', label:'Investment', color: gold, value:[60,70]}, 
+    {id:'free', label:'Free guilt spending', color: volcano, value:[70,100]}
+  ]
+
+  const [ranges, setRanges] = useState<Range[]>([...defaultRanges])
+  const [drag, setDrag] = useState<string>('idle')
+
+  const onChange = (value: number[], idx: number) => {
+    setDrag('active')
+    // Make first or last fixed
+    if(idx === 0 && value[0] !== 0) return 
+    if(idx === ranges.length - 1 && value[1] !== 100) return 
+
+    // Which handle changes
+    const prev = defaultRanges[idx].value
+    console.log("🚀 ~ prev:", prev, value)
+    const delta = [prev[0] - value[0], prev[1] - value[1]]
+    // console.log("🚀 ~ delta:", delta)
+    
+    if(delta[0] !== 0 ) {
+      // console.log("Left")
+    }
+
+    if(delta[1] !== 0 ) {
+      // console.log("Right")
+    }
+
+    // console.log("🚀 ~ delta:", delta)
+    // console.log('onChange: ', idx, value);
+    const _ranges = [...ranges]
+    _ranges[idx].value = value
+    setRanges(_ranges);
+  };
+
+  useEffect(() => {
+    if(drag === 'complete') {
+      defaultRanges = ranges
+    }
+  }, [drag]);
+
+  const onChangeComplete = () => {
+    setDrag('complete')
+  };
+    
   return (
     <div>
-      {categories.map((category, i) => (
-        <ConfigProvider theme={
+      {ranges.map((range, i) => (
+        <ConfigProvider key={i} theme={
           {components: {
             Slider: { 
-              handleColor: category.color[2], 
-              handleActiveColor: category.color[5],
-              dotActiveBorderColor: category.color[2],
-              trackBg: category.color[2], 
-              trackHoverBg: category.color[3],              
+              handleColor: range.color[2], 
+              handleActiveColor: range.color[5],
+              dotActiveBorderColor: range.color[2],
+              trackBg: range.color[2], 
+              trackHoverBg: range.color[3],              
               }
             }
           }} >
-          <div>{category.label}</div>
-          <Slider onChange={(e) => onChange(e, i)} range defaultValue={category.value} />
+          <div>{range.label}</div>
+          <Slider 
+            range
+            onChange={(e) => onChange(e, i)} 
+            onChangeComplete={(e) => onChangeComplete(e, i)} 
+            value={range.value}
+            min={0}
+            max={100}
+          />
         </ConfigProvider>
       ))}
     </div>
